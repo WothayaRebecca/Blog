994013b5a66997637b482f9440b079a063cf8a84
George Karoki <karoki904@gmail.com>
2016-11-16 14:49:23 +0300

    after_adding_const_parent

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'http://vcms.intelligent.co.ke',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'blog',
--- application/models/news_model.php
+++ application/models/news_model.php
@@ -4,7 +4,7 @@ class News_model extends CI_Model
 	
 	public function __construct()
 	{
-		
+		parent::__construct();
 		$this->load->database();
 	}
 	public function get_info($slug=FALSE)
