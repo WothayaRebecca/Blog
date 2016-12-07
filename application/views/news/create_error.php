fc3d3351c70431a8a40a3fc007d63b86e50a1dd3
wothaya <rebeccawanjiru98@gmail.com>
2016-12-07 10:23:30 +0300

    adding error views

---
+++ application/views/news/create_error.php
@@ -0,0 +1,45 @@
+
+<div class="container">
+ <div class="row">
+    <!-- <div class="col-md-2">
+    
+
+
+    </div> -->
+    <div id="row2" class="col-md-8">
+            
+       <?php echo validation_errors() ?>
+       <?php ?>
+       <div class="alert alert-warning" role="alert">
+         <strong>Warning!</strong> Please login to create a blog post.
+       </div>
+      <form action = "create" name="create_form" onsubmit="return validate_create_form();" method = 'post'>
+          <label style="margin-left: 300px; padding-top: 10px" for="title"><b> Title</b> </label>
+          <input id="txtinput" type="input" name="title" ></br><br/>
+          <label style="margin-left: 300px; padding-top: 30px" for="text"><b> Text</b></label>
+          <textarea id="txtinput" name="text"> </textarea></br>
+          <input id="txtinput" style="margin-left: 335px" class="btn btn-default" type="submit"  name="submit" value="create news item"/>
+                
+        </form>
+    </div>
+
+
+ </div> 
+ 
+
+</div>
+  
+    
+
+
+ 
+
+    
+  
+
+
+
+
+
+
+  
---
+++ application/views/pages/loginError.php
@@ -0,0 +1,28 @@
+<div class="container">
+ <div class="row" style="padding-top: 10px; padding-bottom: 10px">
+    <div id="row2" class="col-md-2"></div>
+    <div class="col-md-8" style="border: groove; padding-top: 10px">
+    <?php echo validation_errors()?>
+     <!-- <form action = "Pages/login_validation" method  = 'post'> -->
+     <div class="alert alert-warning" role="alert">
+         <strong>Warning!</strong> Wrong credentials. Please enter the correct Username/Password combination.
+     </div>
+        <form action = "login_validation" name="login_form" onsubmit="return validate_login_form();" method = 'post' style="padding-top: 10px; padding-left: 200px;">          
+          <input id="txtinput" type="input" name="username" placeholder="username" required ></br><br/>         
+          <input id="txtinput" type="password" name="password" placeholder="password" required ></br><br/>
+          <input id="txtinput" style="margin-left: 50px" class="btn btn-default" style="color: RoyalBlue" type="submit"  name="submit" value="Login"/>
+                
+        </form>
+      
+    </div>
+    <div class="col-md-2"></div>
+            
+       
+       
+    
+
+ </div> 
+
+
+</div>
+   
